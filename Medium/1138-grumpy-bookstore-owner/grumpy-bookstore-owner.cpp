ef054a3043353b876f6f91c5bbfd169e29b485c3
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-06-21 11:06:34 +0530

    Time: 12 ms (98.61%) | Memory: 34.2 MB (82.09%) - LeetSync

--- Medium/1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ Medium/1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int n = customers.size();
-        int m = minutes;
-        int count = 0;
-
-        for(int i = 0;i<m;i++){
-            count+=customers[i]*grumpy[i];
-        }
-
+        int n = customers.size(), m = minutes, count = 0;
+        for(int i = 0;i<m;i++) if(grumpy[i]!=0) count+=customers[i];
         int maxUn = count;
-
         for(int i = 1, j = m;i<n-m+1;i++, j++){
-            if(grumpy[i-1]==1){
-                count-=customers[i-1];
-            }
-            
+            if(grumpy[i-1]==1) count-=customers[i-1];
             count+= customers[j]*grumpy[j];
-
             maxUn = max(count, maxUn);
         }
-
-        if(maxUn<0) maxUn = 0;
-
-        for(int i = 0;i<n;i++){
-            maxUn += customers[i] * (1-grumpy[i]);
-        }
-
+        for(int i = 0;i<n;i++) maxUn += customers[i] * (1-grumpy[i]);
         return maxUn;
     }
 };
