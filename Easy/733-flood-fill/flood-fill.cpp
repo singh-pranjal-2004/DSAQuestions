981ce55e4f1b7ea8ca18f5bfb1fecbb4485fb06b
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-06-25 00:31:45 +0530

    Time: 4 ms (83.80%) | Memory: 16.7 MB (56.40%) - LeetSync

---
+++ Easy/733-flood-fill/flood-fill.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void fill(vector<vector<int>>& image, int sr, int sc, int color, int val){
+        if(sr<0 || sc<0 || sr>=image.size() || sc>=image[0].size() || image[sr][sc]!=val || image[sr][sc]==color){
+            return;
+        }
+
+        image[sr][sc] = color;
+
+        fill(image, sr, sc-1, color, val); // left
+        fill(image, sr, sc+1, color, val); //right
+        fill(image, sr+1, sc, color, val); // down
+        fill(image, sr-1, sc, color, val); // up
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int val = image[sr][sc];
+        fill(image, sr, sc, color, val);
+        return image;        
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
