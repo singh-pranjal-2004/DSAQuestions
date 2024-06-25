1ba1f1bdd9f75d129efa702f23721993c13b19da
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-06-25 18:44:20 +0530

    Time: 22 ms (26.81%) | Memory: 24.8 MB (26.67%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,58 +1,25 @@
 class Solution {
 public:
-    bool validator(vector<vector<char>>& board, int sr, int er, int sc, int ec){
-        unordered_set<char>st;
-        for(int row = sr;row<=er;row++){
-            for(int col = sc; col <= ec; col++){
-                if(board[row][col]=='.') continue;
-
-                if(st.find(board[row][col]) != st.end()) return false;
-
-                st.insert(board[row][col]);
-            }
-        }
-        return true;
-    }
-
     bool isValidSudoku(vector<vector<char>>& board) {
-        
-        for(int row = 0; row < 9; row++){
-            unordered_set<char>st;
-            for(int col = 0; col < 9; col++){
-                if(board[row][col] == '.') continue;
+        unordered_set<string>st;
 
-                if(st.find(board[row][col]) != st.end()) return false;
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j]=='.') continue;
 
-                st.insert(board[row][col]);
+                string row = string(1, board[i][j]) + "ROW"+ to_string(i);
+                string col = string(1, board[i][j]) + "COL"+ to_string(j);
+                string box = string(1, board[i][j]) + "BOX"+ to_string(i/3)+ "_" + to_string(j/3);
+
+                if(st.find(row)!=st.end() || st.find(col)!=st.end() || st.find(box)!=st.end()){
+                    return false;
+                }
+
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
             }
         }
-
-        for(int col = 0; col < 9; col++){
-            unordered_set<char>st;
-            for(int row = 0; row < 9; row++){
-                if(board[row][col] == '.') continue;
-
-                if(st.find(board[row][col]) != st.end()) return false;
-
-                st.insert(board[row][col]);
-            }
-        }
-
-        for(int sr = 0; sr < 9; sr += 3){
-            int er = sr+2;
-            for(int sc = 0; sc < 9; sc += 3){
-                int ec = sc+2;
-                if(!validator(board, sr, er, sc, ec)) return false;
-            }
-        }
-
         return true;
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
