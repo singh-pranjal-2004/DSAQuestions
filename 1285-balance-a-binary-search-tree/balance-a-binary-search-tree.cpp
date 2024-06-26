767c07219200ee76ef23d7ded3deba6c91e60d2d
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-06-26 07:10:24 +0530

    Time: 1239 ms (5.03%) | Memory: 717.7 MB (5.39%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -10,24 +10,38 @@
  * };
  */
 class Solution {
-    vector<TreeNode*>inOrder;
-    void inOrderTree(TreeNode* root){
-        if(!root)return;
-        inOrderTree(root->left);
-        inOrder.push_back(root);
-        inOrderTree(root->right);
+public:
+    void storer(vector<TreeNode*>& vect, TreeNode* root){
+        if(!root) return;
+
+        storer(vect, root->left);
+        vect.push_back(root);
+        storer(vect, root->right);
     }
-    TreeNode* balanced(int start,int end){
-        if(start>end)return NULL;
-        int mid=(start+end)/2;
-        TreeNode* root=inOrder[mid];
-        root->left=balanced(start,mid-1);
-        root->right=balanced(mid+1,end);
+
+    TreeNode* solver(vector<TreeNode*> vect, int start, int end){
+        if(start > end) return NULL;
+
+        int mid = start + (end - start)/2;
+
+        TreeNode* root = vect[mid];
+        root->left = solver(vect, start, mid-1);
+        root->right = solver(vect, mid+1, end);
+
         return root;
     }
-public:
     TreeNode* balanceBST(TreeNode* root) {
-        inOrderTree(root);
-        return balanced(0,inOrder.size()-1);
+        vector<TreeNode*> vect;
+        storer(vect, root);
+        return solver(vect, 0, vect.size()-1);
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+
+
