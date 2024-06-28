e6e0db6c44664ae04f6d51a05ec1c1b572eb3384
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-06-28 19:06:00 +0530

    Time: 288 ms (66.21%) | Memory: 116.3 MB (89.48%) - LeetSync

---
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        vector<int>arr(n, 0);
+        for(auto &vec: roads){
+            int u = vec[0];
+            int v = vec[1];
+
+            arr[u]++;
+            arr[v]++;
+        }
+
+        sort(arr.begin(), arr.end());
+        for(int i = 0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+
+        long long sum = 0;
+        for(long long i = 1;i<=arr.size();i++){
+            sum+=(i*arr[i-1]);
+        }
+        return sum;
+    }
+};
