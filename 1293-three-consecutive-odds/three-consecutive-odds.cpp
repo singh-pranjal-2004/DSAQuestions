9f59fe91ab0a3e01ea204cf7ab501d0e2e7c69e1
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-01 08:35:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (8.09%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-        int i = 0, j = 0;
-        while(i<arr.size() && j<arr.size()){
-            if(arr[j]%2==1){
-                if(j-i==2){
-                    return true;
-                }
-                j++;
-            }else{
-                j++;
-                i = j;
-            }
+        int count = 0;
+        for(int i = 0;i<arr.size();i++){
+            if(arr[i]%2==1){
+                count++;
+                if(count==3) return true;
+            }else count = 0;
         }
+
         return false;
     }
 };
