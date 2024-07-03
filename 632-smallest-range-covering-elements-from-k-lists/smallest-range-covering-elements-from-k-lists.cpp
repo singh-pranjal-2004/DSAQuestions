911d1a1d7eeac3b1faceb37ff0f09b439df4eca2
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-03 09:58:59 +0530

    Time: 44 ms (61.49%) | Memory: 22.3 MB (37.17%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -13,24 +13,17 @@ public:
         }
     };
 
-    class compare1{
+    class compare{
         public:
         bool operator()(node* a, node* b){
             return a->data > b->data;
         }
     };
 
-    class compare2{
-        public:
-        bool operator()(node* a, node* b){
-            return a->data < b->data;
-        }
-    };
     vector<int> smallestRange(vector<vector<int>>& nums) {
         vector<int> ans;
         int minAns = INT_MAX;
-        // priority_queue<node*, vector<node*>, compare2> maxHeap;
-        priority_queue<node*, vector<node*>, compare1> minHeap;
+        priority_queue<node*, vector<node*>, compare> minHeap;
         int n = nums.size();
         int maxi = INT_MIN;
 
@@ -38,18 +31,15 @@ public:
             node* temp = new node(nums[i][0], i, 0);
             minHeap.push(temp);
             maxi = max(maxi, nums[i][0]);
-            // maxHeap.push(temp);
         }
 
         while(minHeap.size()==n){
             node* t1 = minHeap.top();
-            // node* t2 = maxHeap.top();
             minHeap.pop();
 
             int t1d = t1->data;
             int row = t1->i;
             int col = t1->j;
-            // int t2d = t2->data;
 
             if(maxi-t1d < minAns){
                 minAns = maxi-t1d;
@@ -61,7 +51,6 @@ public:
             if(col+1 < nums[row].size()){
                 node* next = new node(nums[row][col+1], row, col+1);
                 minHeap.push(next);
-                // maxHeap.push(next);
                 maxi = max(maxi, nums[row][col+1]);
             }
         }
