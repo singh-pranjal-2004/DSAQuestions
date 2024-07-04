bbbc2cd4a58bac21f4ae001a274faf6ca03cceb4
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-04 17:25:15 +0530

    Time: 10 ms (99.09%) | Memory: 27.4 MB (40.16%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -7,3 +7,10 @@ public:
         reverse(nums.begin(), nums.end());
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
