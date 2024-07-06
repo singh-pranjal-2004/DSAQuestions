0078beb2ca8ae150eb0da841aa0d467569b22b94
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-06 14:46:51 +0530

    Time: 7 ms (95.91%) | Memory: 19.3 MB (29.79%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int xor1 = 0;
-        for(auto i: nums){
-            xor1 = xor1^i;
-        }
+        for(auto i: nums) xor1 ^= i;
         return xor1;
     }
 };
