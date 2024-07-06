fb21082e787d7db0f12077418332a9f2a3248c4a
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-06 11:32:05 +0530

    Time: 422 ms (67.96%) | Memory: 259.6 MB (60.25%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -8,58 +8,24 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-static const int __ = [](){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    //cout.tie(nullptr);
-    return 0;
-}();
-
-int init = [] {
-    ofstream out("user.out");
-    cout.rdbuf(out.rdbuf());
-    for (string str; getline(cin, str);) {
-        istringstream ss(str); ss.ignore(3);
-        ostringstream os;
-        int sum = 0;
-        bool first = true;
-        cout << '[';
-        for (int i; ss >> i; ss.ignore()) {
-            if (!i) {
-                os << (first ? "" : ",") << sum;
-                sum = 0;
-                first = false;
-            } else {
-                sum += i;
-            }
-        }
-        os << ']';
-        cout << os.str() << '\n';
-    }
-    exit(0);
-    return 0;
-}();
-
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
-        ListNode* ans = new ListNode();
-        ListNode* curr = ans;
-
         head = head->next;
-        int sum = 0;
-        while(head!=NULL){
-            if(head->val == 0){
-                curr->next = new ListNode(sum);
-                curr = curr->next;
-                sum = 0;
-            }
-
-            sum+=head->val;
-            head = head->next;
+        if(head==NULL){
+            return head;
         }
 
-        return ans->next;
+        ListNode* temp = head;
+        int sum = 0;
+        while(temp->val!=0){
+            sum+=temp->val;
+            temp = temp->next;
+        }
+
+        head->val = sum;
+        head->next = mergeNodes(temp); // Recursion Leap of Faith
+
+        return head;
     }
 };
