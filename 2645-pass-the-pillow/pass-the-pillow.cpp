13e4cb88dcf8564bfdf207dc3882c276445c08a6
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-07 11:37:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (31.20%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        int ans = 0;
-        for(int i = 0; i <= time; i++){
-            ans++;
-            if(ans==-1 || ans == n) ans*=-1;
-        }
-        return abs(ans);
+        int fullRound = time / (n-1);
+        int timeLeft = time % (n-1);
+
+        if(fullRound%2==0){
+            return timeLeft+1;
+        }else return n-timeLeft;
     }
 };
