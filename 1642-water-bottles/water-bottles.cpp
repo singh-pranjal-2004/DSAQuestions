ddefe736842eceddaf374234573d2ea3e353f7a3
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-07 10:29:54 +0530

    Time: 2 ms (53.84%) | Memory: 7.3 MB (30.43%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int ans = 0;
-        int rem = 0;
-        ans+=numBottles;
-        while(numBottles>=numExchange){
-            ans += (numBottles/numExchange);
-            rem += (numBottles % numExchange);
-            numBottles/=numExchange;
-            numBottles += rem;
-            rem = 0;
-        }
-        return ans;
+        return numBottles + ((numBottles-1) / (numExchange-1));
     }
 };
 
