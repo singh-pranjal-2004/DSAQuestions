532a58857e0db60da1fd65d79e9e5ace3b745c95
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-08 17:59:23 +0530

    Time: 2 ms (54.02%) | Memory: 7.1 MB (30.83%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -10,3 +10,10 @@ public:
         return drinked + numBottles;
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
