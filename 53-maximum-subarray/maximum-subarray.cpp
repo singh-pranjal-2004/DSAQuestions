aa40aec834845eab49dd95be0df15278a4a6a5b0
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-08 08:39:42 +0530

    Time: 30 ms (99.75%) | Memory: 70 MB (92.57%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -6,10 +6,17 @@ public:
         for(int i = 0; i < nums.size(); i++){
             sum+=nums[i];
             ans = max(ans, sum);
-            if(sum < 0){
+            if(sum < 0){ // Kadane's Algorithm - Make sum 0 if the sum is a negative number
                 sum = 0;
             }
         }
         return ans;
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
