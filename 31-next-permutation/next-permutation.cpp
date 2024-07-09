96a2e8f4471e1edc5e846eafa406721a749b6936
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-09 09:27:25 +0530

    Time: 4 ms (55.27%) | Memory: 14.6 MB (15.95%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        // next_permutation(nums.begin(), nums.end()); // STL for next_permutation
+
+        int n = nums.size();
+        int index = -1, mini = nums[n-1], miniIndex = n-1;
+        for(index = n-2; index >= 0; index--){
+            if(nums[index] < nums[index+1]){
+                break;
+            }
+        }
+
+        cout<<index;
+
+        if(index == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for(int i = n-1; i > index; i--){
+            if(nums[i] > nums[index]){
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+
+        reverse(nums.begin()+index+1, nums.end());
     }
 };
