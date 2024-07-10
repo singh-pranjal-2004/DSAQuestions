1d35fb00211311a73c6e3b5972c5aa44f5f30929
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-10 18:51:59 +0530

    Time: 154 ms (50.44%) | Memory: 76.4 MB (83.97%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        double ans = 0;
+        double total = 0;
+        int n = customers.size();
+
+        total += customers[0][0];
+        for(int i = 0; i < n; i++){
+            if(total < customers[i][0]){
+                total = customers[i][0];
+            }
+            total += customers[i][1];
+
+            ans = ans + (total - customers[i][0]);
+        }
+
+        return ans/n;
+    }
+};
