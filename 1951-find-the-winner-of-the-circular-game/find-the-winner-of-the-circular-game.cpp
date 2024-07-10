8e4db3af4ac8d6710bee64ebd98d1b74e06d4c74
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-10 17:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (44.95%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
-    static int findTheWinner(int n, int k) {
-       if (n==1) return 1;
-       return (findTheWinner(n-1, k)+(k-1))%n+1;
+    int findTheWinner(int n, int k) {
+        vector<int> v;
+        for(int i = 1; i <= n; i++){
+            v.push_back(i);
+        }
+
+        int index = 0;
+        while(n>1){
+            index = (index + k - 1) % n;
+            v.erase(v.begin() + index);
+            n--;
+        }
+        
+        return v[0];
     }
 };
