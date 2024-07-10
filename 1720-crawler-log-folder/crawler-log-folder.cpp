695578d00de67574abb3ee0bf90ff345a09fe1de
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-10 18:16:26 +0530

    Time: 4 ms (67.13%) | Memory: 13.8 MB (84.88%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int c = 0;
+        for(auto str: logs){
+            if(str=="../") (c>0)?c--:c=0;
+            else if(str=="./") continue;
+            else c++;
+        }
+
+        return (c>=0)? c : 0;
+    }
+};
