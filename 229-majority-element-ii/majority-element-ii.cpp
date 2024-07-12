54c6e4c07eb1d33a6dd462962fffabc05315786c
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-12 07:31:39 +0530

    Time: 14 ms (16.89%) | Memory: 18.6 MB (30.22%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int number1 = 0, number2 = 0, count1 = 0, count2 = 0, n = nums.size();
         vector<int> ans;
-        unordered_map<int, int>mp;
-        
-        for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
+
+        for(int i = 0; i < n; i++){
+            if(count1 == 0 && number2 != nums[i]){
+                count1 = 1;
+                number1 = nums[i];
+            }else if(count2 == 0 && number1 != nums[i]){
+                count2 = 1;
+                number2 = nums[i];
+            }else if(number1 == nums[i]){
+                count1++;
+            }else if(number2 == nums[i]){
+                count2++;
+            }else{
+                count1--;
+                count2--;
+            }
         }
 
-        int threshold = nums.size() / 3;
-
-        for(auto it: mp){
-            int number = it.first;
-            int count = it.second;
-
-            if(count > threshold) ans.push_back(number);
+        int threshold = n / 3;
+        count1 = 0, count2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == number1) count1++;
+            else if(nums[i] == number2) count2++;
         }
-        
+
+        if(count1 > threshold) ans.push_back(number1);
+        if(count2 > threshold) ans.push_back(number2);
+
         return ans;
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
