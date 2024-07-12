d9f4a3866a58a5272000692998e356451dc6bfca
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-12 07:25:27 +0530

    Time: 12 ms (29.78%) | Memory: 19.9 MB (7.28%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        unordered_map<int, int>mp;
+        
+        for(int i = 0; i < nums.size(); i++){
+            mp[nums[i]]++;
+        }
+
+        int threshold = nums.size() / 3;
+
+        for(auto it: mp){
+            int number = it.first;
+            int count = it.second;
+
+            if(count > threshold) ans.push_back(number);
+        }
+        
+        return ans;
+    }
+};
