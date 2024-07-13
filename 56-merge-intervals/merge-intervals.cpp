03b51db29dadefa0521d83e0610b64b432285f11
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-13 14:40:44 +0530

    Time: 19 ms (85.49%) | Memory: 22.9 MB (28.19%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -6,18 +6,11 @@ public:
 	    int n = arr.size();
 
 	    for(int i = 0; i < n; i++){
-	    	int start = arr[i][0];
-	    	int end = arr[i][1];
-
-	    	if(!ans.empty() && end <= ans.back()[1]) continue;
-
-	    	for(int j = i + 1; j < n; j++){
-	    		if(arr[j][0] <= end){
-	    			end = max(end, arr[j][1]);
-	    		}else break;
-	    	}
-
-	    	ans.push_back({start, end});
+	    	if(ans.empty() || arr[i][0] > ans.back()[1]){
+                ans.push_back(arr[i]);
+            }else{
+                ans.back()[1] = max(ans.back()[1], arr[i][1]);
+            }
 	    }
 
 	    return ans;
