3c3cf54ffbc2f7e2da1badd64d37ac6eef896ad4
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-13 14:33:03 +0530

    Time: 22 ms (68.10%) | Memory: 22.9 MB (28.19%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,22 +4,29 @@ public:
         vector<vector<int>> ans;
 	    sort(arr.begin(), arr.end());
 	    int n = arr.size();
-    
+
 	    for(int i = 0; i < n; i++){
 	    	int start = arr[i][0];
 	    	int end = arr[i][1];
-    
+
 	    	if(!ans.empty() && end <= ans.back()[1]) continue;
-    
+
 	    	for(int j = i + 1; j < n; j++){
 	    		if(arr[j][0] <= end){
 	    			end = max(end, arr[j][1]);
 	    		}else break;
 	    	}
-    
+
 	    	ans.push_back({start, end});
 	    }
-    
+
 	    return ans;
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
