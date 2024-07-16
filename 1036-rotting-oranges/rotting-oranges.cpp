9bf6d582f19e57f093f6e75f1c0a6c4e0da46791
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-07-16 08:03:02 +0530

    Time: 8 ms (25.08%) | Memory: 17.2 MB (6.31%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,33 +1,29 @@
 class Solution {
 private:
-    void bfs(vector<vector<int>>& grid, vector<vector<int>>& vis, queue<vector<int>>& q, int& minute, int m, int n){
+    void bfs(vector<vector<int>>& grid, vector<vector<int>>& vis, queue<vector<int>>& q, int& minute, int m, int n, int& freshOrange){
         while(!q.empty()){
-            int s = q.size();
-            while(s--){
-                int row = q.front()[0];
-                int col = q.front()[1];
-                int prevTime = q.front()[2];
-                int delRow[] = {-1, +1, 0, 0};
-                int delCol[] = {0, 0, -1, +1};
-
-                for(int i = 0; i < 4; i++){
-                    int r = row + delRow[i];
-                    int c = col + delCol[i];
-
-                    if(r >= 0 && r < m && c >= 0 && c < n && !vis[r][c] && grid[r][c] == 1){
-                        vis[r][c] = 1;
-                        grid[r][c] = 2;
-                        q.push({r, c, prevTime+1});
-                    }
+            int row = q.front()[0];
+            int col = q.front()[1];
+            int prevTime = q.front()[2];
+            q.pop();
+            int delRow[] = {-1, +1, 0, 0};
+            int delCol[] = {0, 0, -1, +1};
+            for(int i = 0; i < 4; i++){
+                int r = row + delRow[i];
+                int c = col + delCol[i];
+                if(r >= 0 && r < m && c >= 0 && c < n && !vis[r][c] && grid[r][c] == 1){
+                    vis[r][c] = 1;
+                    grid[r][c] = 2;
+                    q.push({r, c, prevTime+1});
+                    freshOrange--;
                 }
-                q.pop();
-                minute = max(minute, prevTime);
             }
+            minute = max(minute, prevTime);
         }
     }
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+        int m = grid.size(), n = grid[0].size(), freshOrange = 0;
         vector<vector<int>> vis(m, vector<int>(n, 0));
         queue<vector<int>>q;
 
@@ -36,22 +32,18 @@ public:
                 if(grid[i][j] == 2){
                     vis[i][j] = 1;
                     q.push({i, j, 0});
+                }else if(grid[i][j] == 1){
+                    freshOrange++;
                 }
             }
         }
 
         int minute = 0;
-        bfs(grid, vis, q, minute, m, n);
+        bfs(grid, vis, q, minute, m, n, freshOrange);
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
-            }
-        }
+        if(!freshOrange) return minute;
 
-        return minute;
+        return -1;
     }
 };
 
