78965f55ca8b11592217ed273b6db7685881f931
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-08-01 23:16:58 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (16.71%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
-        int ans = 0;
-        for (auto& x : details) {
-            int age = stoi(x.substr(11, 2));
-            ans += age > 60;
+        int count = 0;
+        for(auto str: details){
+            int age = stoi(str.substr(11, 2));
+            if(age > 60) count++;
         }
-        return ans;
+        return count;
     }
 };
