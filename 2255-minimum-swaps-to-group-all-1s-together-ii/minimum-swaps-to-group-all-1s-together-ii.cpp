229167fd73740552cab8c613c585d858b0c810ba
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-08-02 21:50:52 +0530

    Time: 62 ms (83.89%) | Memory: 83.1 MB (57.56%) - LeetSync

---
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int totalOnes = 0, ans = INT_MAX;
+        int n = nums.size();
+        for(auto i: nums){
+            if(i) totalOnes++;
+        }
+
+        int currentOnes = 0;
+        for(int j = 0; j < totalOnes; j++){
+            currentOnes += nums[j];
+        }
+
+        for(int i = 0; i < n; i++){
+            ans = min(ans, totalOnes - currentOnes);
+            currentOnes -= nums[i];
+            currentOnes += nums[(i+totalOnes) % n];
+        }
+
+        return ans;
+    }
+};
