2c52b3819556ddf7ce40af5beb430910bdbd0c2c
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-08-05 14:03:59 +0530

    Time: 75 ms (82.03%) | Memory: 11.3 MB (83.33%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -5,33 +5,39 @@ public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
         priority_queue<P, vector<P>, greater<P>> pq;
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             pq.push({nums[i], i});
         }
 
         int result = 0;
 
-        for(int count = 1; count <= right; count++){
+        for (int count = 1; count <= right; count++) {
             auto p = pq.top();
-        pq.pop();
+            pq.pop();
 
-        int sum = p.first;
-        int idx = p.second;
+            int sum = p.first;
+            int idx = p.second;
 
-        if(count >= left){
-            result = (result + sum) % m;
+            if (count >= left) {
+                result = (result + sum) % m;
+            }
+
+            int newIdx = idx + 1;
+            P new_pair;
+            if (newIdx < n) {
+                new_pair.first = sum + nums[newIdx];
+                new_pair.second = newIdx;
+                pq.push(new_pair);
+            }
         }
 
-        int newIdx = idx + 1;
-        P new_pair;
-        if(newIdx < n){
-            new_pair.first = sum + nums[newIdx];
-            new_pair.second = newIdx;
-            pq.push(new_pair);
-        }
-        }
-        
         return result;
     }
-
 };
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
