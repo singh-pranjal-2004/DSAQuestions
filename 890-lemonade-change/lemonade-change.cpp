b32edef91d99821e1825018749eaf9d0f9d392c2
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-08-15 08:15:08 +0530

    Time: 79 ms (26.27%) | Memory: 86.1 MB (32.71%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int changeAvailable = 0;
+        int five = 0, ten = 0, twenty = 0;
+
+        for(auto i: bills){
+            if(i == 5) five++;
+            else if(i == 10) ten++;
+            else twenty++;
+
+            if(i == 10){
+                if(five > 0) five--;
+                else return false; 
+            } 
+            else if(i == 20){
+                if(five > 0 && ten > 0){
+                    five--;
+                    ten--;
+                }else if(five >= 3) five-=3;
+                else return false;
+            } 
+        }
+
+        return true;
+    }
+};
