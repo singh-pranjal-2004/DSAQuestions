09fe18198954df56af94bc48597bde506099ec20
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-08-22 18:40:25 +0530

    Time: 3 ms (99.59%) | Memory: 14.4 MB (25.48%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) { 
+        int tRows = board.size();
+        int tCols = board[0].size();
+        queue<pair<int, int>>q;
+        vector<vector<int>>vis(tRows, vector<int>(tCols, 0));
+
+
+        for(int i = 0; i < tRows; i++){
+            if(board[i][0] == 'O' && !vis[i][0]){
+                q.push({i, 0});
+                vis[i][0] = 1;
+            }
+            
+            if(board[i][tCols-1] == 'O' && !vis[i][tCols-1]){
+                q.push({i, tCols-1});
+                vis[i][tCols-1] = 1;
+            }
+        }
+
+        for(int i = 0; i < tCols; i++){
+            if(board[0][i] == 'O' && !vis[0][i]){
+                q.push({0, i});
+                vis[0][i] = 1;
+            }
+            
+            if(board[tRows-1][i] == 'O' && !vis[tRows-1][i]){
+                q.push({tRows-1, i});
+                vis[tRows-1][i] = 1;
+            }
+        }
+
+        int delRow[4] = {-1, 0, 1, 0};
+        int delCol[4] = {0, -1, 0, 1};
+
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for(int i = 0; i < 4; i++){
+                int newRow = r + delRow[i];
+                int newCol = c + delCol[i];
+
+                if(newRow >= 0 && newCol >= 0 && newRow < tRows && newCol < tCols && board[newRow][newCol] == 'O' && !vis[newRow][newCol] ){
+                    q.push({newRow, newCol});
+                    vis[newRow][newCol] = 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < tRows; i++){
+            for(int j = 0; j < tCols; j++){
+                if(board[i][j] == 'O' && !vis[i][j]) board[i][j] = 'X';
+            }
+        }
+    }
+};
