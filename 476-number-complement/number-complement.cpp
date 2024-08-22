4953214411b425c7349acdf6c24a37c61d000c96
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-08-22 15:02:54 +0530

    Time: 3 ms (41.07%) | Memory: 7.6 MB (33.08%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string decToBinComp(int num) {
+        string str;
+        while (num) {
+            if (num & 1) {
+                str += '0';
+            } else {
+                str += '1';
+            }
+            num >>= 1;
+        }
+
+        return str;
+    }
+
+    long binToDec(string str) {
+        long ans = 0;
+        long base = 1;
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            int n = str[i] - '0';
+            ans += (n * base);
+            base *= 2;
+        }
+
+        return ans;
+    }
+
+    int findComplement(int num) {
+        string str = decToBinComp(num);
+        reverse(str.begin(), str.end());
+
+        long ans = binToDec(str);
+        cout << ans;
+        return ans;
+    }
+};
