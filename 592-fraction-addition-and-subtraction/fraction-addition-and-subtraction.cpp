b9b87ce85c7b41cc6395e121bf219eca98eaf903
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-08-23 16:05:31 +0530

    Time: 3 ms (50.39%) | Memory: 7.8 MB (58.91%) - LeetSync

---
+++ 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string fractionAddition(string expression) {
+        int s = expression.size();
+        int i = 0;
+        int ntr = 0, dtr = 1;
+        while(i < s){
+            int sign = 1;
+            int n = 0, d = 0;
+
+            if(expression[i] == '-'){
+                sign = -1;
+                i++;
+            }else if(expression[i] == '+'){
+                i++;
+            }
+
+            while(isdigit(expression[i])){
+                n = n * 10 + (expression[i]-'0');
+                i++;
+            }
+            n *= sign;
+
+            i++;
+
+            while(isdigit(expression[i])){
+                d = d * 10 + (expression[i]-'0');
+                i++;
+            }
+
+            ntr = ntr * d;
+            n = n * dtr;
+            ntr += n;
+            dtr = dtr * d;
+
+            int gd = gcd(abs(ntr), dtr);
+            ntr /= gd;
+            dtr /= gd;
+            
+        }
+
+        return to_string(ntr) + "/" + to_string(dtr);
+    }
+};
