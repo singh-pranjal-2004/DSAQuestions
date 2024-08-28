4e9873e11937543cc60b9a272518aa1628f7eb84
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-08-28 16:46:09 +0530

    Time: 151 ms (29.45%) | Memory: 90.2 MB (30.25%) - LeetSync

---
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& land, vector<vector<int>>& vis, int row, int col, int& ti, int& tj, int& bi, int& bj){
+        vis[row][col] = 1;
+
+        if(row < ti){
+            ti = row;
+        }
+
+        if(col < tj){
+            tj = col;
+        }
+
+        if(row > bi){
+            bi = row;
+        }
+
+        if(col > bj){
+            bj = col;
+        }
+        
+        int delRow[4] = {-1, 0, 1, 0};
+        int delCol[4] = {0, 1, 0, -1};
+
+        for(int i = 0; i < 4; i++){
+            int newR = row + delRow[i];
+            int newC = col + delCol[i];
+
+            if(newR >= 0 && newC >= 0 && newR < land.size() && newC < land[0].size() && land[newR][newC] == 1 && !vis[newR][newC]){
+                dfs(land, vis, newR, newC, ti, tj, bi, bj);
+            }
+        }
+    }
+
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        vector<vector<int>>ans;
+        int n = land.size(), m = land[0].size();
+        vector<vector<int>>vis(n, vector<int>(m, 0));
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(land[i][j] == 1 && !vis[i][j]){
+                    int ti = i, tj = j, bi = i, bj = j;
+                    dfs(land, vis, i, j, ti, tj, bi, bj);
+                    vector<int>temp;
+                    temp.push_back(ti);
+                    temp.push_back(tj);
+                    temp.push_back(bi);
+                    temp.push_back(bj);
+                    ans.push_back(temp);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
