2320b2af4eea8cfec699238d5afbb203f65542e4
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-08-28 10:57:56 +0530

    Time: 217 ms (58.83%) | Memory: 102.2 MB (40.53%) - LeetSync

---
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2,
+             vector<vector<int>>& vis, int delRow[], int delCol[], int row,
+             int col, int n, int m, bool& isValid) {
+        vis[row][col] = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int newRow = row + delRow[i];
+            int newCol = col + delCol[i];
+
+            if (newRow >= 0 && newCol >= 0 && newRow < n && newCol < m &&
+                grid2[newRow][newCol] == 1 && !vis[newRow][newCol]) {
+                if (grid1[newRow][newCol] == 0)
+                    isValid = false;
+                dfs(grid1, grid2, vis, delRow, delCol, newRow, newCol, n, m,
+                    isValid);
+            }
+        }
+    }
+
+    int countSubIslands(vector<vector<int>>& grid1,
+                        vector<vector<int>>& grid2) {
+        int n = grid2.size();
+        int m = grid2[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int delRow[4] = {-1, 0, 1, 0};
+        int delCol[4] = {0, 1, 0, -1};
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                bool isValid = true;
+                if (grid2[i][j] == 1 && !vis[i][j]) {
+                    if(grid1[i][j] == 0){
+                        isValid = false;
+                    }
+                    
+                    dfs(grid1, grid2, vis, delRow, delCol, i, j, n, m, isValid);
+                    if (isValid){
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+};
