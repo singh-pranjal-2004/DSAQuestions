1b668d0920ee8eec23ebc6a2b8e111f052e031b4
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-09-05 15:24:46 +0530

    Time: 4 ms (72.69%) | Memory: 10.7 MB (68.58%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -30,3 +30,11 @@ public:
         return ans.empty()?"/":ans;
     }
 };
+
+const static auto fast = []
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
