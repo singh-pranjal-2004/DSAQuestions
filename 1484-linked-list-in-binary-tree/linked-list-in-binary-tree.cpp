69f183f5c9132f3fb4b11fb42336a5e121e22dc1
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-09-07 10:48:01 +0530

    Time: 26 ms (48.97%) | Memory: 31 MB (18.42%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
@@ -22,8 +22,8 @@
 class Solution {
 public:
     bool dfs(ListNode* head, ListNode* curr, TreeNode* root){
-        if(curr == NULL) return true;
-        if(root == NULL) return false;
+        if(!curr) return true;
+        if(!root) return false;
 
         if(root -> val == curr->val) curr = curr -> next;
         else if(root -> val == head -> val) head = head -> next;
