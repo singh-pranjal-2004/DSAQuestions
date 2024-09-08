73310ebd04c517ffc97956e8ad9a839291420e39
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-09-08 09:16:54 +0530

    Time: 8 ms (26.89%) | Memory: 13.9 MB (61.52%) - LeetSync

--- 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
@@ -40,7 +40,15 @@ public:
             curr -> next = NULL;
             curr = temp;
         }
-        
+
         return ans;
     }
 };
+
+const static auto fast = []
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
