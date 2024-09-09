75f5de601c5d0605e6b18695d385532a4940ea4f
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-09-09 08:39:22 +0530

    Time: 173 ms (31.09%) | Memory: 130.7 MB (17.37%) - LeetSync

---
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        int left = 0, right = n-1;
+        int top = 0, bottom = m-1;
+        vector<vector<int>>ans(m, vector<int>(n, -1));
+        ListNode* curr = head;
+        while(curr!=NULL){
+            
+            // top
+            for(int i = left; i <= right && curr; i++){
+                ans[top][i] = curr -> val;
+                curr = curr -> next;
+            }
+            top++;
+            
+            // right
+            for(int i = top; i <= bottom && curr; i++){
+                ans[i][right] = curr -> val;
+                curr = curr -> next;
+            }
+            right--;
+
+            // bottom
+            for(int i = right; i >= left && curr; i--){
+                ans[bottom][i] = curr -> val;
+                curr = curr -> next;
+            }
+            bottom--;
+
+            // left
+            for(int i = bottom; i >= top && curr; i--){
+                ans[i][left] = curr -> val;
+                curr = curr -> next;
+            }
+            left++;
+        }
+
+        return ans;
+    }
+};
