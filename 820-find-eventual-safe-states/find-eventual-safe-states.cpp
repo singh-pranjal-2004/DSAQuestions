e315392f4c5dc3c751440a77ee77082787425987
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-09-12 08:47:23 +0530

    Time: 130 ms (65.55%) | Memory: 62.6 MB (62.90%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -32,3 +32,11 @@ public:
         return topo;
     }
 };
+
+const static auto fast = []
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
