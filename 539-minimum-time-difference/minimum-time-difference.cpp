c0e5b3f43e09c437d2ec84c68a439457810078f6
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-09-16 10:34:06 +0530

    Time: 11 ms (69.95%) | Memory: 17.6 MB (77.18%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -26,3 +26,11 @@ public:
         return lastDiff;
     }
 };
+
+const static auto fast = []
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
