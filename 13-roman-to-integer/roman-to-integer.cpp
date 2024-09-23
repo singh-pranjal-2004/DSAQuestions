929bde6d13cbfb35747dafa5f94e81fbf274a09c
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-09-23 16:16:33 +0530

    Time: 8 ms (70.27%) | Memory: 14.2 MB (24.60%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int ans = 0;
+        unordered_map<char, int>mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
 
+        int ans = 0;
         for(int i = 0; i < s.length(); i++){
-            if(s[i] == 'I') ans += 1;
-            else if(s[i] == 'V'){
-                if(i-1>=0 && s[i-1] == 'I') ans+=3;
-                else ans += 5;
-            }else if(s[i] == 'X'){
-                if(i-1>=0 && s[i-1] == 'I') ans+=8;
-                else ans += 10;
-            }else if(s[i] == 'L'){
-                if(i-1>=0 && s[i-1] == 'X') ans+=30;
-                else ans += 50;
-            }else if(s[i] == 'C'){
-                if(i-1>=0 && s[i-1] == 'X') ans+=80;
-                else ans += 100;
-            }else if(s[i] == 'D'){
-                if(i-1>=0 && s[i-1] == 'C') ans+=300;
-                else ans += 500;
-            }else if(s[i] == 'M'){
-                if(i-1>=0 && s[i-1] == 'C') ans+=800;
-                else ans += 1000;
+            if(mp[s[i]] < mp[s[i+1]]){
+                ans -= mp[s[i]];
+            }else{
+                ans += mp[s[i]];
             }
         }
 
