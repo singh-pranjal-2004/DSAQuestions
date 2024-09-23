dde78acc69d4f43db925415252995024bf9e8cf0
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-09-23 19:53:54 +0530

    Time: 4 ms (35.28%) | Memory: 12.4 MB (63.77%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -14,11 +14,3 @@ public:
         return low;
     }
 };
-
-const static auto fast = []
-{
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-}();
