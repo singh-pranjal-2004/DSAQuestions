230df77dd803c2b547883bc14d93674a322894f5
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-10-07 10:59:55 +0530

    Time: 12 ms (15.77%) | Memory: 11.7 MB (31.24%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -3,40 +3,17 @@ public:
     int minLength(string s) {
         stack<char> st;
 
-        for (auto str : s)
-            st.push(str);
-
-        string str;
-        char prev = '-';
-        while (!st.empty()) {
-            char ch = st.top();
-
-            if(prev == '-' && (ch == 'B' || ch == 'D')){
-                prev = ch;
-                str = ch + str;
-            }else if(prev != '-'){
-                if((ch == 'A' && prev == 'B') || (ch == 'C' && prev == 'D')){
-                    str.erase(0, 1);
-                    if(str[0] == 'D' || str[0] == 'B') prev = str[0];
-                    else prev = '-';
-                }else {
-                    prev = ch;
-                    str = ch + str;
-                }
-            }else str = ch + str;
-
-            st.pop();
+        for(auto ch: s){
+            if(st.empty()){
+                st.push(ch);
+            }
+            else{
+                char tp = st.top();
+                if((ch == 'B' && tp == 'A') || (ch == 'D' && tp == 'C')) st.pop();
+                else st.push(ch);
+            } 
         }
 
-        return str.length();
+        return st.size();
     }
 };
-
-
-const static auto fast = []
-{
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-}();
