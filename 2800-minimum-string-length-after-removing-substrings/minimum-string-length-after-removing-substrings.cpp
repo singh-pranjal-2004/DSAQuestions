b6a9295485b0bf780f708ab041d460ef81aff1e4
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-10-07 11:03:43 +0530

    Time: 3 ms (89.38%) | Memory: 11.6 MB (61.24%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -3,15 +3,16 @@ public:
     int minLength(string s) {
         stack<char> st;
 
-        for(auto ch: s){
-            if(st.empty()){
+        for (auto ch : s) {
+            if (st.empty()) {
                 st.push(ch);
+                continue;
             }
-            else{
-                char tp = st.top();
-                if((ch == 'B' && tp == 'A') || (ch == 'D' && tp == 'C')) st.pop();
-                else st.push(ch);
-            } 
+            char tp = st.top();
+            if ((ch == 'B' && tp == 'A') || (ch == 'D' && tp == 'C'))
+                st.pop();
+            else
+                st.push(ch);
         }
 
         return st.size();
