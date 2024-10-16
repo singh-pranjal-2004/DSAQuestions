c9df9c8628b608b8608a6dd9270b9577ccbbcec5
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-10-16 15:33:28 +0530

    Time: 3 ms (43.32%) | Memory: 7.7 MB (64.09%) - LeetSync

---
+++ 1304-longest-happy-string/longest-happy-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        priority_queue<pair<int, char>>pq;
+        if(a > 0) pq.push({a, 'a'});
+        if(b > 0) pq.push({b, 'b'});
+        if(c > 0) pq.push({c, 'c'});
+
+        string result = "";
+
+        while(!pq.empty()){
+            int count1 = pq.top().first;
+            char c1 = pq.top().second;
+            pq.pop();
+
+            if(result.size() >= 2 && result.back() == c1 && result[result.size()-2] == c1){
+                if(pq.empty()) break;
+
+                int count2 = pq.top().first;
+                int c2 = pq.top().second;
+                pq.pop();
+
+                result += c2;
+                count2--;
+                if(count2 > 0) pq.push({count2, c2});
+                if(count1 > 0) pq.push({count1, c1});
+            }else{
+                result += c1;
+                count1--;
+
+                if(count1 > 0) pq.push({count1, c1});
+            }
+        }
+        return result;
+    }
+};
