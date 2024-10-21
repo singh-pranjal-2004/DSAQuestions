8a78328eebc61da3aba912b68521cae5c7e83dbc
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-10-21 11:21:26 +0530

    Time: 224 ms (60.82%) | Memory: 47.7 MB (81.58%) - LeetSync

---
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solver(unordered_set<string>& ust, string& s, int idx, int currCount, int &maxCount){
+        if(idx >= s.length()){
+            maxCount = max(currCount, maxCount);
+            return;
+        }
+
+        for(int j = idx; j < s.length(); j++){
+            string sub = s.substr(idx, j-idx+1);
+            if(ust.find(sub) == ust.end()){
+                ust.insert(sub);
+                solver(ust, s, j + 1, currCount+1, maxCount);
+                ust.erase(sub);
+            }
+        }
+    }
+
+    int maxUniqueSplit(string s) {
+        unordered_set<string>ust;
+        int currCount = 0, maxCount = 0, idx = 0;
+
+        solver(ust, s, idx, currCount, maxCount);
+
+        return maxCount;
+    }
+};
