4afec099c953c96e3f88032bf9c308e569b6a614
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-10-24 02:14:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Geek Jump/geek-jump.cpp
+++ Difficulty: Easy/Geek Jump/geek-jump.cpp
@@ -6,24 +6,42 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int solver(vector<int>& height, int n, vector<int>&dp){
-        if (n == 0) return 0;
+    // int solver(vector<int>& height, int n, vector<int>&dp){
+    //     if (n == 0) return 0;
         
-        if(dp[n] != -1) return dp[n];
+    //     if(dp[n] != -1) return dp[n];
         
-        int two = INT_MAX;
-        int one = solver(height, n-1, dp) + abs(height[n] - height[n-1]);
-        if(n > 1) two = solver(height, n-2, dp) + abs(height[n] - height[n-2]);
+    //     int two = INT_MAX;
+    //     int one = solver(height, n-1, dp) + abs(height[n] - height[n-1]);
+    //     if(n > 1) two = solver(height, n-2, dp) + abs(height[n] - height[n-2]);
         
-        return dp[n] = min(one, two);
-    }
+    //     return dp[n] = min(one, two);
+    // }
     
+    // int minimumEnergy(vector<int>& height, int n) {
+    //     // Code here
+    //     vector<int>dp(n, -1);
+    //     return solver(height, n-1, dp);
+    // }
+    
+    
+    // ************************************************
     int minimumEnergy(vector<int>& height, int n) {
         // Code here
-        vector<int>dp(n, -1);
-        return solver(height, n-1, dp);
+        vector<int>dp(n, 0);
+        
+        for(int i = 1; i < n; i++){
+            int fs = dp[i-1] + abs(height[i] - height[i-1]);
+            int ss = INT_MAX;
+            if(i > 1) ss = dp[i-2] + abs(height[i] - height[i-2]);
+            
+            dp[i] = min(fs, ss);
+        }
+        
+        return dp[n-1];
     }
 };
+
 
 //{ Driver Code Starts.
 
