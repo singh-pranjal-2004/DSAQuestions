ec195ad3c16418e5e16d334f9f8d53af68150bf4
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-10-23 16:16:07 +0530

    Time: 25 ms (98.61%) | Memory: 61.1 MB (92.67%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -10,3 +10,11 @@ public:
         return false;
     }
 };
+
+const static auto fast = []
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
