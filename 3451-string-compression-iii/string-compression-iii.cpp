612496ffd35be085afdd9647e02897e99e1a381a
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-11-04 10:23:29 +0530

    Time: 7 ms (96.58%) | Memory: 28 MB (77.20%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -23,3 +23,11 @@ public:
         return comp;
     }
 };
+
+const static auto fast = []
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
