0107299f1dac960cd5b36ab5a2a5a0a2bc11c8d0
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-11-07 14:54:24 +0530

    Time: 8 ms (23.31%) | Memory: 12.8 MB (20.75%) - LeetSync

--- 2020-remove-one-element-to-make-the-array-strictly-increasing/remove-one-element-to-make-the-array-strictly-increasing.cpp
+++ 2020-remove-one-element-to-make-the-array-strictly-increasing/remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool canBeIncreasing(vector<int>& nums) {
-        
         for(int i = 0; i < nums.size(); i++){
             bool check = true;
             int pre = (i==0) ? 0 : nums[0];
@@ -25,3 +24,11 @@ public:
         return false;
     }
 };
+
+const static auto fast = []
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
