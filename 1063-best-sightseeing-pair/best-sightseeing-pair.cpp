d39c536121aac5d09944ee9d6b96b019286d1de1
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-12-28 01:14:09 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (32.48%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -3,15 +3,12 @@ public:
     int maxScoreSightseeingPair(vector<int>& values) {
         int n = values.size();
 
-        vector<int>maxVal(n, INT_MIN);
-        maxVal[0] = values[0];
-        for(int i = 1; i < n; i++){
-            maxVal[i] = max(maxVal[i-1], values[i]+i);
-        }
-
         int maxAns = INT_MIN;
+        int maxTillNow = values[0];
         for(int i = 1; i < n; i++){
-            maxAns = max(maxAns, maxVal[i-1] + values[i] - i);
+            maxAns = max(maxAns, maxTillNow + values[i] - i);
+
+            maxTillNow = max(maxTillNow, values[i]+i);
         }
 
         return maxAns;
