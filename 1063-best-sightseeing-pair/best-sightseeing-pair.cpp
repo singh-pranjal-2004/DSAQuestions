69c89c6bbb82bb00bc2b26f5107f5323cff54ebd
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2024-12-28 01:10:12 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (13.16%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -17,3 +17,11 @@ public:
         return maxAns;
     }
 };
+
+const static auto fast = []
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
