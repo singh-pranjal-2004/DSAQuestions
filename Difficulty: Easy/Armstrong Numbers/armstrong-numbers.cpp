61b3bce4fca35a6b59afeeb2443d8de5a721bb65
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-01-23 11:32:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    bool armstrongNumber(int n) {
+        // code here
+        int sum = 0, temp = n;
+        
+        while(temp > 0){
+            int t = temp % 10;
+            temp /= 10;
+            
+            sum += pow(t, 3);
+        }
+        
+        return sum == n;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        bool flag = ob.armstrongNumber(n);
+        if (flag) {
+            cout << "true" << endl;
+        } else {
+            cout << "false" << endl;
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
