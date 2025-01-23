00aeea3c332e9f8f25b981fda92f4cd04c3ac560
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-01-23 23:49:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (86.18%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int solver(vector<int>& nums, vector<int>& dp, int start, int end){
-        dp[start] = nums[start];
+    int solver(vector<int>& nums, int start, int end){
+        int prev1 = nums[start];
+        int prev2 = 0;
 
         for(int i = start+1; i <= end; i++){
-            int take = nums[i] + (i-2 >= start ? dp[i-2] : 0);
-            int notTake = dp[i-1];
+            int take = nums[i] + (i-2 >= start ? prev2 : 0);
+            int notTake = prev1;
 
-            dp[i] = max(take, notTake);
+            prev2 = prev1;
+            prev1 = max(take, notTake);
         }
 
-        return dp[end];
+        return prev1;
     }
 
     int rob(vector<int>& nums) {
@@ -18,10 +20,8 @@ public:
         if(n == 1) return nums[0];
         if(n == 2) return max(nums[0], nums[1]);
 
-        vector<int>dp1(n, 0);
-        vector<int>dp2(n, 0);
-        int ignoreFirst = solver(nums, dp1, 1, n-1);
-        int ignoreLast = solver(nums, dp2, 0, n-2);
+        int ignoreFirst = solver(nums, 1, n-1);
+        int ignoreLast = solver(nums, 0, n-2);
 
         return max(ignoreFirst, ignoreLast);
     }
