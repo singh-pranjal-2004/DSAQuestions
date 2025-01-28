5e2356f8fa18efb9cc709410a19f4f1e90ffcd1a
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-01-28 10:16:02 +0530

    Time: 3 ms (31.25%) | Memory: 15 MB (38.77%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int solver(vector<vector<int>>& matrix, vector<vector<int>>& dp, int i, int j, int n){
-        if(j < 0 || j >= n) return 1e9;
-        if(i == n-1) return matrix[i][j];
-
-        if(dp[i][j] != -1e9) return dp[i][j];
-
-        int left = matrix[i][j] + solver(matrix, dp, i+1, j-1, n);
-        int right = matrix[i][j] + solver(matrix, dp, i+1, j+1, n);
-
-        int down = matrix[i][j] + solver(matrix, dp, i+1, j, n);
-
-        return dp[i][j] = min(left, min(right, down));
-    }
-
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int mini = 1e9;
-        vector<vector<int>>dp(n, vector<int>(n, -1e9));
+        vector<vector<int>>dp(n, vector<int>(n, 0));
+
+        for(int i = 0; i < n; i++) dp[0][i] = matrix[0][i];
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                int left = 1e9, right = 1e9, top = 1e9;
+                if(j > 0) left = matrix[i][j] + dp[i-1][j-1];
+                if(j < n-1) right = matrix[i][j] + dp[i-1][j+1];
+
+                top = matrix[i][j] + dp[i-1][j];
+
+                dp[i][j] = min(top, min(left, right));
+            }
+        }
 
         for(int i = 0; i < n; i++){
-            int ans = solver(matrix, dp, 0, i, n);
-            mini = min(mini, ans);
+            mini = min(mini, dp[n-1][i]);
         }
 
         return mini;
