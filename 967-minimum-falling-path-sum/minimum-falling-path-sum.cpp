e3d930365d7a39c2ead0f8098ce90ecc98cf3123
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-01-28 09:42:21 +0530

    Time: 4 ms (18.93%) | Memory: 17.5 MB (5.55%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solver(vector<vector<int>>& matrix, vector<vector<int>>& dp, int i, int j, int n){
+        if(j < 0 || j >= n) return 1e9;
+        if(i == n-1) return matrix[i][j];
+
+        if(dp[i][j] != -100000) return dp[i][j];
+
+        int left = matrix[i][j] + solver(matrix, dp, i+1, j-1, n);
+        int right = matrix[i][j] + solver(matrix, dp, i+1, j+1, n);
+
+        int down = matrix[i][j] + solver(matrix, dp, i+1, j, n);
+
+        return dp[i][j] = min(left, min(right, down));
+    }
+
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int mini = 1e9;
+        vector<vector<int>>dp(101, vector<int>(101, -100000));
+
+        for(int i = 0; i < n; i++){
+            int ans = solver(matrix, dp, 0, i, n);
+            mini = min(mini, ans);
+        }
+
+        return mini;
+    }
+};
