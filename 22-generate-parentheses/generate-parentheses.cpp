0ae08336183cbd1864f22d1db08e299b85a00412
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-02-06 16:25:17 +0530

    Time: 6 ms (15.18%) | Memory: 15.7 MB (35.14%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    void solver(vector<string>& ans, string str, int open, int close, int n){
-        if(open == n && close == n){
+    void solver(vector<string>& ans, string str, int open, int close, int n) {
+        if(open == n && close == n) {
             ans.push_back(str);
             return;
         }
 
-        if(open < n) solver(ans, str + '(', open + 1, close, n);
-        if(open > close) solver(ans, str + ')', open, close + 1, n);
+        if(open < n) 
+            solver(ans, str+"(", open+1, close, n);
+
+        if(close < open)
+            solver(ans, str+")", open, close+1, n);
     }
 
     vector<string> generateParenthesis(int n) {
         vector<string>ans;
-        if(n == 0) return ans;
+        if(n == 0)
+            return ans;
 
         solver(ans, "(", 1, 0, n);
-        
+
         return ans;
     }
 };
-
-const static auto fast = []
-{
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-}();
