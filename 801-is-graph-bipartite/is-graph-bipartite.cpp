d8a2d924a646e7c2a593f2ca22e002e2a0fd12a7
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-02-07 17:54:48 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (76.52%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    bool dfs(int start, vector<vector<int>>& graph, vector<int>& vis, int col){
-        vis[start] = !col;
+    bool dfs(vector<vector<int>>& graph, vector<int>& color, int u, int col) {
+        color[u] = col;
 
-        for(auto i: graph[start]){
-            if(vis[i] == -1){
-                if(dfs(i, graph, vis, !col) == false) return false;
-            }else if(vis[i] != -1 && vis[i] == vis[start]) return false;
+        for(auto& v: graph[u]) {
+            int colV = 1 - col;
+            if(color[v] == col) {
+                return false;
+            }else if(color[v] == -1 && !dfs(graph, color, v, colV)) {
+                return false;
+            }
         }
 
         return true;
     }
-    
+
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int>vis(n, -1);
-        
-        for(int i = 0; i < n; i++){
-            if(vis[i] == -1){
-                if(dfs(i, graph, vis, 0) == false) return false;
+        vector<int>color(n, -1);
+
+        for(int i = 0; i < n; i++) {
+            if(color[i] == -1 && !dfs(graph, color, i, 0)) {
+                return false;
             }
         }
 
         return true;
     }
 };
-
-const static auto fast = []
-{
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-}();
