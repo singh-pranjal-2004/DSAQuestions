95493e45ad17754380201df3bcbdde085e6d07e1
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-02-07 16:50:39 +0530

    Time: 1 ms (84.51%) | Memory: 18.2 MB (61.89%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,40 +1,47 @@
 class Solution {
 public:
+    bool dfs(vector<vector<int>>& adj, vector<bool>& vis, vector<bool>& inRecursion, stack<int>& st, int u) {
+        vis[u] = true;
+        inRecursion[u] = true;
+
+        for(auto& v: adj[u]) {
+            if(!vis[v] && dfs(adj, vis, inRecursion, st, v)) {
+                return true;
+            }else if(inRecursion[v]) {
+                return true;
+            }
+        }
+
+        st.push(u);
+        inRecursion[u] = false;
+        return false;
+    }
+
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
-        vector<int>inDegree(numCourses);
 
         for(auto& vec: prerequisites) {
             int a = vec[0];
             int b = vec[1];
 
             adj[b].push_back(a);
-            inDegree[a]++;
         }
 
-        vector<int> topo;
-        queue<int>q;
+        vector<bool>vis(numCourses, false);
+        vector<bool>inRecursion(numCourses, false);
+        stack<int>st;
         for(int i = 0; i < numCourses; i++) {
-            if(inDegree[i] == 0) {
-                q.push(i);
-            }
+            if(!vis[i] && dfs(adj, vis, inRecursion, st, i))
+                return {};
         }
 
-        while(!q.empty()) {
-            int u = q.front();
-            q.pop();
-            topo.push_back(u);
-
-            for(auto& v: adj[u]) {
-                inDegree[v]--;
-                if(inDegree[v] == 0) 
-                    q.push(v);
-            }
+        vector<int>ans;
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
         }
 
-        if(topo.size() == numCourses)
-            return topo;
+        return ans;
 
-        return {};
     }
 };
