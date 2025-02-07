e37dc21c2cab7fb9069b44a7fb8f7788a5c475e1
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-02-07 15:07:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (86.55%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
+    bool dfs(vector<vector<int>>& adj, vector<bool>& vis, vector<bool>& inRecur, int u) {
+        vis[u] = true;
+        inRecur[u] = true;
+
+        for(auto& v: adj[u]) {
+            if(!vis[v]) {
+                if(dfs(adj, vis, inRecur, v))
+                    return true;
+            }else if(inRecur[v]) {
+                return true;
+            }
+        }
+
+        inRecur[u] = false;
+        return false;
+    }
+
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
-        vector<int>inDegree(numCourses);
 
         for(auto& vec: prerequisites) {
             int a = vec[0];
             int b = vec[1];
 
             adj[b].push_back(a);
-            inDegree[a]++;
         }
 
-        queue<int>q;
-        int count = 0;
+        vector<bool>vis(numCourses, false);
+        vector<bool>inRecur(numCourses, false);
+
         for(int i = 0; i < numCourses; i++) {
-            if(inDegree[i] == 0) {
-                q.push(i);
-                count++;
-            }
+            if(!vis[i])
+                if(dfs(adj, vis, inRecur, i) == true) return false;
         }
 
-        while(!q.empty()) {
-            int u = q.front();
-            q.pop();
-
-            for(auto& v: adj[u]) {
-                inDegree[v]--;
-                if(inDegree[v] == 0) {
-                    q.push(v);
-                    count++;
-                }
-            }
-        }
-
-        if(count == numCourses)
-            return true;
-
-        return false;
+        return true;
     }
 };
