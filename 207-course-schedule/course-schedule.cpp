239ab5e01d8ff7ebae348aeb30538e39fd168ad7
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-02-07 12:09:45 +0530

    Time: 7 ms (53.45%) | Memory: 19.2 MB (76.82%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
-        vector<int>inDegree(numCourses, 0);
+        vector<vector<int>>adj(numCourses);
+        vector<int>inDegree(numCourses);
 
-        for(auto i: prerequisites){
-            adj[i[1]].push_back(i[0]);
-            inDegree[i[0]]++;
+        for(auto& vec: prerequisites) {
+            int a = vec[0];
+            int b = vec[1];
+
+            adj[b].push_back(a);
+            inDegree[a]++;
         }
 
         queue<int>q;
-        for(int i = 0; i < numCourses; i++){
-            if(inDegree[i] == 0) q.push(i);
-        }
-
         int count = 0;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            count++;
-
-            for(auto adjacent: adj[node]){
-                inDegree[adjacent]--;
-                if(inDegree[adjacent] == 0) q.push(adjacent);
+        for(int i = 0; i < numCourses; i++) {
+            if(inDegree[i] == 0) {
+                q.push(i);
+                count++;
             }
         }
 
-        return count == numCourses;
+        while(!q.empty()) {
+            int u = q.front();
+            q.pop();
 
+            for(auto& v: adj[u]) {
+                inDegree[v]--;
+                if(inDegree[v] == 0) {
+                    q.push(v);
+                    count++;
+                }
+            }
+        }
 
+        if(count == numCourses)
+            return true;
+
+        return false;
     }
 };
-
-const static auto fast = []
-{
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-}();
