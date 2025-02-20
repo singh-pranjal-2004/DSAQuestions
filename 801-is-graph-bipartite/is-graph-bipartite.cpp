cbe7cf5c7eee6eaeb5d664642846f6201c4148b9
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-02-20 15:42:45 +0530

    Time: 107 ms (5.90%) | Memory: 29.4 MB (5.12%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph, vector<int>& color, int u, int col) {
-        color[u] = col;
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>>adj(n);
 
-        for(auto& v: graph[u]) {
-            int colV = 1 - col;
-            if(color[v] == col) {
-                return false;
-            }else if(color[v] == -1 && !dfs(graph, color, v, colV)) {
-                return false;
+        for(int u = 0; u < n; u++) {
+            for(int i = 0; i < graph[u].size(); i++) {
+                int v = graph[u][i]; 
+                adj[u].push_back(v);
+                adj[v].push_back(u);
             }
         }
 
-        return true;
-    }
-
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int>color(n, -1);
+        vector<int>vis(n, -1);
 
         for(int i = 0; i < n; i++) {
-            if(color[i] == -1 && !dfs(graph, color, i, 0)) {
-                return false;
+            if(vis[i] == -1) {
+                queue<pair<int, int>>q;
+                q.push({i, 0});
+                vis[i] = 0;
+
+                while(!q.empty()) {
+                    int v = q.front().first;
+                    int col = q.front().second;
+                    vis[v] = col;
+                    q.pop();
+
+                    for(auto u: adj[v]) {
+                        if(vis[u] == col) return false;
+
+                        if(vis[u] == -1) {
+                            q.push({u, 1-col});
+                        }
+                    }
+                }
             }
         }
 
