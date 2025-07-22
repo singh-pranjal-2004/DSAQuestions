6adf51ae33671ec52bd0ba9182743f0a5be2924b
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-07-22 07:30:38 +0530

    Time: 346 ms (57.02%) | Memory: 8.8 MB (62.46%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    void solveSudoku(vector<vector<char>>& board) {
-        solver(board);
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c || board[i][col] == c)
+                return false;
+
+            if(board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == c) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    bool solver(vector<vector<char>>& board) {
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                if(board[i][j] == '.') {
-                    for(char c = '1'; c <= '9'; c++) {
-                        if(possible(board, i, j, c)){
+    bool solver(vector<vector<char>>& board, int row) {
+        for (int i = row; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
                             board[i][j] = c;
-                            if(solver(board)){
+                            if (solver(board, i))
                                 return true;
-                            }else{
-                                board[i][j] = '.';
-                            }
+                            
+                            board[i][j] = '.';
                         }
                     }
                     return false;
@@ -25,23 +32,5 @@ public:
         return true;
     }
 
-    bool possible(vector<vector<char>>& board, int row, int col, char c){
-        for(int i = 0; i < 9; i++){
-            if(board[row][i] == c || board[i][col] == c){
-                return false;
-            }
-
-            if(board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == c){
-                return false;
-            }
-        }
-        return true;
-    }
+    void solveSudoku(vector<vector<char>>& board) { solver(board, 0); }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
