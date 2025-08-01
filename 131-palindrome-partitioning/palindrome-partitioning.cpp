132eff765dec60ade1d3f78dd328ead39896acf4
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-08-01 07:07:32 +0530

    Time: 50 ms (56.49%) | Memory: 57.5 MB (61.99%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool checker(string str) {
         int i = 0, j = str.length()-1;
-        while(i <= j) {
+        while(i < j) {
             if(str[i]!=str[j])
                 return false;
 
