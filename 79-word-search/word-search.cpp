d7753e979c84a72c5c868b49300023e3284a07e8
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-08-01 08:03:29 +0530

    Time: 379 ms (49.54%) | Memory: 11.8 MB (17.74%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool solver(vector<vector<char>>& board, vector<vector<bool>>& vis, string word, int i, int j, int m, int n, int wIdx) {
+        if(wIdx >= word.length())
+            return true;
+
+        vis[i][j] = true;
+        int newRow[4] = {-1, 0, 1, 0};
+        int newCol[4] = {0, 1, 0, -1};
+
+        for(int k = 0; k < 4; k++) {
+            int nr = i+newRow[k], nc = j+newCol[k];
+            if(nr >= 0 && nr < m && nc >= 0 && nc < n && board[nr][nc] == word[wIdx] && !vis[nr][nc]) {
+                if(solver(board, vis, word, nr, nc, m, n, wIdx+1)){
+                    vis[i][j] = false;
+                    return true;
+                }
+                
+            }
+        }
+
+        vis[i][j] = false;
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size(), n = board[0].size();
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(word[0] == board[i][j]) {
+                    if(word.length()==1)
+                        return true;
+
+                    if(solver(board, vis, word, i, j, m, n, 1))
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
