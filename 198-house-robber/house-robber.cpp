da4ac21a7ffa567d1e35ef8cc3e7826b5c38f5d3
Pranjal Ashok Singh <118631231+singh-pranjal-2004@users.noreply.github.com>
2025-08-30 17:53:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (71.70%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
+    int solver(vector<int>& nums, vector<int>& dp, int n, int i) {
+        if(i >= n)
+            return 0;
+
+        if(dp[i] != -1)
+            return dp[i];
+
+        int take = nums[i] + solver(nums, dp, n, i+2);
+        int notTake = solver(nums, dp, n, i+1);
+
+        return dp[i] = max(take, notTake);
+    }
+
     int rob(vector<int>& nums) {
         int n = nums.size();
-
-        int curr = nums[0];
-        int prev = 0;
-
-        for(int i = 1; i < n; i++){
-            int a = nums[i] + (i>=2 ? prev : 0);
-            int b = curr;
-
-            prev = curr;
-            curr = max(a, b);
-        }
-
-        return curr;
+        vector<int>dp(n+1, -1);
+        return solver(nums, dp, n, 0);
     }
 };
